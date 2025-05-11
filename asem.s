8f8e6a4577d5a06f96d30b7cf7c6e567367b4089
hyperingenious <skbmasale941@gmail.com>
2025-05-11 07:21:17 +0530

    HElloworld

--- asem.s
+++ asem.s
@@ -2,4 +2,18 @@
 .intel_syntax noprefix
 
 _start:
+	// sys write
+	mov rax, 1
+	mov rdi, 1
+	lea rsi, [hello_world]
+	mov rdx, 14
+	syscall
+
+	// sys exit
+	mov rax, 60
+	mov rdi, 69
+	syscall
+
+hello_world:
+	.asciz "Hello, World!\n"
 
